f52ffbc8fc5626b867d1d64ffe1d2bedd6859428
Guillermo Bernal <gbernal@media.mit.edu>
2017-06-15 00:41:56 -0400

    Added Rotation Class

---
+++ BuildingSpace/Source/BuildingSpace/OpenDoor.cpp
@@ -0,0 +1,38 @@
+// Copyright Guillermo Bernal 2017
+
+#include "OpenDoor.h"
+#include "Gameframework/Actor.h"
+
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+    FString Owner = GetOwner()->GetName();
+    FString OwnRotation = GetOwner()->GetTransform().GetRotation().ToString();
+    UE_LOG(LogTemp, Warning, TEXT(" %s is at Rotation %s "), *Owner, *OwnRotation);
+
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
