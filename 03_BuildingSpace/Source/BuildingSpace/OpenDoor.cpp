646e611e47f799fd6d13008813996a7d6bedd519
Guillermo Bernal <gbernal@mit.edu>
2017-06-19 11:41:55 -0400

    deletedOldAddedNew
    Mac and Windows were not p[laying nice

---
+++ 03_BuildingSpace/Source/BuildingSpace/OpenDoor.cpp
@@ -0,0 +1,42 @@
+// Copyright Guillermo Bernal 2017
+
+#include "OpenDoor.h"
+#include "Gameframework/Actor.h"
+
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+    //FInd the owner actor
+    AActor* Owner = GetOwner();
+    
+    //Create a rotator
+    FRotator NewRotation = FRotator(0.0f, -0.60f, 0.0f);
+
+    //Set The door Rotation
+    Owner->SetActorRotation(NewRotation);
+
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
